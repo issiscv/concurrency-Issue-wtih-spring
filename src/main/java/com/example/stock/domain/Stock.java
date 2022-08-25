e7bd4681a408320bb1c29c64174d0212e0641bda
issiscv <issiscv46@gmail.com>
2022-08-25 14:55:49 +0900

    FEAT: 프로젝트 예제 코드 작성

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,37 @@
+package com.example.stock.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Stock {
+
+    @Id @GeneratedValue
+    @Column(name = "stock_id")
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("foo");
+        }
+
+        this.quantity = this.quantity - quantity;
+    }
+
+}
---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,23 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findById(id).orElseThrow();
+
+        stock.decrease(quantity);
+    }
+
+}
